6f5e518b8358c7b5954fcc7fd72036c6635e44fe
ryanmg-dev <adrianmauricio337@gmail.com>
2021-03-06 20:01:25 -0500

    Se realizo la animacion principal de los tokens

--- js/picked.js
+++ js/picked.js
@@ -4,6 +4,8 @@ const tokens = [
     {name: "rock", element: document.getElementById('rock')}
 ];
 
+let focus;
+
 //Añadirles un evento click a los tokens 
 tokens.forEach(function (elemento, index, arreglo) {
     elemento.element.addEventListener('click', 
@@ -12,12 +14,27 @@ tokens.forEach(function (elemento, index, arreglo) {
         if(elemento.name == 'scissor'){
             elemento.element.classList.toggle('picked-scissor');            
             arreglo[0].element.classList.toggle('picked-scissor--paper');
-            arreglo[2].element.classList.toggle('picked-scissor--rock');
+            arreglo[2].element.classList.toggle('picked-scissor--rock');            
         }         
         else if(elemento.name == 'rock'){
             elemento.element.classList.toggle('picked-rock');            
             arreglo[0].element.classList.toggle('picked-rock--paper');
-            arreglo[1].element.classList.toggle('picked-rock--scissor');
+            arreglo[1].element.classList.toggle('picked-rock--scissor');            
+        }else{
+            document.getElementById('step-1').classList.add('novisible');
         }
+        focus = elemento.element;        
+        setTimeout(  
+            function (){
+                document.getElementById('step-1').classList.add('hide');
+                stepSecond();
+            }                                          
+            ,2200);        
     });    
 });
+
+function stepSecond (){    
+    document.getElementById('step-2').appendChild(focus);
+    document.getElementById('step-2').classList.remove('hide');    
+}
+
